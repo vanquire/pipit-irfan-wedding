ddef9ec72249b71264030295e1edbe1992e00468
vanquire <38317742+vanquire@users.noreply.github.com>
2026-08-14 21:16:12 +0800

    Add invitation functionality

---
+++ data/guests.js
@@ -0,0 +1,9 @@
+/*
+ * Edit this list. `id` is used in the link: ?to=id
+ * Keep IDs lowercase, short, and URL-safe.
+ */
+window.WEDDING_GUESTS = {
+  "andi-keluarga": { name: "Bapak Andi & Keluarga", maxGuests: 2 },
+  "siti-keluarga": { name: "Ibu Siti & Keluarga", maxGuests: 2 },
+  "sahabat-rina": { name: "Rina", maxGuests: 1 }
+};
